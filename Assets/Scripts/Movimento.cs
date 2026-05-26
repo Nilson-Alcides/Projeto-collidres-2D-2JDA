c3f8550b14acba950e087863a8c90a9898b6f445
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-05-26 12:25:26 -0300

    OnTrigger coletando celular

--- Assets/Scripts/Movimento.cs
+++ Assets/Scripts/Movimento.cs
@@ -2,63 +2,92 @@ using UnityEngine;
 
 public class Movimento : MonoBehaviour
 {
-    private Rigidbody2D myRigidbody;
-
+    private Rigidbody2D myRigidbody2D;
     private float horizontal;
 
-    [SerializeField] private float movimentoSpeed;
+    private bool facingRight;
 
-    private bool facingRigth; // Virado para direita
+    [SerializeField] private float movimentoSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
         movimentoSpeed = 10;
 
-        facingRigth = true;
+        facingRight = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        //HandMovimento(horizontal);
-        //Debug.Log(horizontal);
-    }
-    void HandMovimento(float horizontal)
-    {
-       myRigidbody.linearVelocity = new Vector2(horizontal * movimentoSpeed, myRigidbody.linearVelocity.y);
-        Debug.Log(horizontal);
 
     }
+    // Melhoria FixedUpdate consome menos recurso da maquina
     void FixedUpdate()
     {
         HandMovimento(horizontal);
 
         Flip(horizontal);
     }
+
+    void HandMovimento(float horizontal)
+    {
+        myRigidbody2D.linearVelocity = new Vector2(horizontal * movimentoSpeed, myRigidbody2D.linearVelocity.y);
+        //Debug.Log(horizontal);
+    }
     void Flip(float horizontal)
     {
-        if (horizontal > 0 && !facingRigth || horizontal < 0 && facingRigth)
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
         {
-            facingRigth = !facingRigth;
+            facingRight = !facingRight;
             Vector2 theScale = transform.localScale;
 
             //theScale.x *= theScale.x * -1;
             theScale.x *= -1;
 
             transform.localScale = theScale;
-           // Debug.Log("O personagem virou");
+            Debug.Log("O personagem virou");
         }
     }
-    void OnCollisionEnter2D()
+    void OnCollisionEnter2D(Collision2D colisao)
     {
-        Debug.Log("Colidiu com o objeto");
-    }
-    void OnCollisionExit2D()
-    {
-        Debug.Log("Deixou de Colidir com o objeto");
+        if (colisao.gameObject.tag == "Objetos")
+        {
+            Debug.Log("Colidiu com o objeto " + colisao.gameObject.name);
+        }
+
     }
 
+    void OnCollisionExit2D(Collision2D colisao)
+    {
+
+        Debug.Log("Saiu da colisão com o objeto");
+    }
+
+    void OnCollisionStay2D()
+    {
+        Debug.Log("Colidindo com o objeto");
+    }
+    // Função que detecta colisão com objeto TRIGGER
+    void OnTriggerEnter2D(Collider2D colisao)
+    {
+        //Debug.Log("Colidiu com objeto TRIGGER");
+        if (colisao.gameObject.tag == "Objetos")
+        {
+            Destroy(colisao.gameObject);
+        }
+    }
+    // Função que detecta quando o player deixou de colidir com objeto TRIGGER
+    void OnTriggerExit2D(Collider2D colisao)
+    {
+        // Debug.Log("Deixou de colidir com objeto TRIGGER");
+    }
+    // Função para detectar colisão continua com objeto TRIGGER
+    void OnTriggerStay2D(Collider2D colisao)
+    {
+        // Debug.Log("Player continua colidindo  com objeto TRIGGER");
+    }
 }
